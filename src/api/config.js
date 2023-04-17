06307de20c17c111a4f230a1fa06200f65f81be7
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2023-04-16 23:16:41 -0300

    final version

--- src/api/config.js
+++ src/api/config.js
@@ -2,5 +2,5 @@ export const APISettings = {
   headers: new Headers({
     Accept: "application/json",
   }),
-  baseURL: "http://i-07e925caec86c9ddc:443",
+  baseURL: "http://i-07e925caec86c9ddc:80/api",
 };
